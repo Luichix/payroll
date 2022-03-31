f4058eb3681daf8c91835f93a9b41d24e23c82a1
luichix <luichix.rex@gmail.com>
2022-03-31 12:56:23 -0600

    test crud

--- app.js
+++ app.js
@@ -7,29 +7,20 @@ const usersRouter = require('./controllers/user')
 const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
 const mysql = require('mysql')
+const { insert } = require('./models/operation')
 
-logger.info('connecting to', config.MONGODB_URI)
-logger.info('connecting to', config.MySQL.database.host)
+logger.info('connecting to', config.MySQL.database.database)
 
 
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB', error.message)
-  })
+const connection = mysql.createConnection(config.MySQL.database)
 
-const conexion = mysql.createConnection(config.MySQL.database)
-
-conexion.connect(function(error) {
+connection.connect(function(error) {
   if(error){
     logger.error('error connecting to MySQL', error.stack)
     return
   }
-  logger.info('connecting to MySQL', conexion.threadId)
+  logger.info('connecting to MySQL', connection.threadId)
 })
 
 app.use(cors())
@@ -38,6 +29,13 @@ app.use(express.json( { limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(middleware.requestLogger)
 
+
+app.get('/insert', (request, response) => {
+  insert(connection,{ username: 'Manu', password: 'kirito', fullname: 'Manu Rex' },
+    result => {
+      response.json(result)
+    })
+})
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
--- utils/config.js
+++ utils/config.js
@@ -2,12 +2,6 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 
-let MONGODB_URI = process.env.MONGODB_URI
-
-if(process.env.NODE_ENV === 'test'){
-  MONGODB_URI = process.env.TEST_MONGODB_URI
-}
-
 const cloudinary = require('cloudinary').v2
 
 cloudinary.config({
@@ -27,7 +21,6 @@ const MySQL = {
 
 module.exports = {
   PORT,
-  MONGODB_URI,
   cloudinary,
   MySQL
 }
