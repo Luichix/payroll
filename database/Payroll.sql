d517eb9f933c79f54b1f4217cc234bbf1e2c9e9a
luichix <luichix.rex@gmail.com>
2022-04-01 16:28:36 -0600

    -ajust query

--- database/Payroll.sql
+++ database/Payroll.sql
@@ -2,11 +2,11 @@ CREATE DATABASE database_payroll;
 
 USE database_payroll;
 
-CREATE TABLE `database_payroll`. `users`(
+CREATE TABLE `database_payroll`. `PAYROLL`(
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `username` VARCHAR (45) NOT NULL,
   `password` VARCHAR(60) NOT NULL,
   `fullname` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`));
 
-DESCRIBE users;
+DESCRIBE PAYROLL;
